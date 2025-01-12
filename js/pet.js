31628e61272c961152b9b1413b0c915386520ed5
Soyab-Aktar <iamsoyabcode23@gmail.com>
2025-01-12 10:50:44 +0530

    Category Button Created

--- js/pet.js
+++ js/pet.js
@@ -1,13 +1,24 @@
-const loadCategory = () =>{
-    fetch('https://openapi.programming-hero.com/api/peddy/categories')
-    .then(Response=> Response.json())
-    .then(data => displayCategory(data.categories))
-    .catch((error)=>console.log(error));
-}
+const loadCategory = () => {
+  fetch("https://openapi.programming-hero.com/api/peddy/categories")
+    .then((Response) => Response.json())
+    .then((data) => displayCategory(data.categories))
+    .catch((error) => console.log(error));
+};
 
-const displayCategory=(categories)=>{
-    const categoryContainer= document.getElementById('category-container')
-}
+const displayCategory = (categories) => {
+  const categoryContainer = document.getElementById("category-container");
+  categories.forEach((item) => {
+    console.log(item);
 
+    const btnContainer = document.createElement("div");
+    btnContainer.innerHTML = `
+        <button class="flex items-center justify-center gap-2  px-4 py-2 rounded-full bg-sky-100 border border-teal-600 btn-wide w-full">
+          <img class="w-12 h-12" src="${item.category_icon}" />
+          <span class="font-medium text-teal-900">${item.category}</span>
+        </button>
+        `;
+    categoryContainer.append(btnContainer);
+  });
+};
 
 loadCategory();
